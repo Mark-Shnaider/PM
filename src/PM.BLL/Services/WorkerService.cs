a7f281ebc61755191ca3a913f60332b04efed0b8
shnayder <mark.shnayder@sibres.com>
2022-03-28 19:39:01 +0700

    Add BLL project

---
+++ src/PM.BLL/Services/WorkerService.cs
@@ -0,0 +1,13 @@
+﻿using PM.DAL.Domain.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.BLL.Services
+{
+    public class WorkerService : IWorkerService
+    {
+    }
+}
