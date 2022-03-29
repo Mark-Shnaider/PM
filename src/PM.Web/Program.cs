05405b19d8a2f69eb56d4838e4b1230fdc0074e7
shnayder <mark.shnayder@sibres.com>
2022-03-29 22:16:48 +0700

    Add changes to program.cs file

--- src/PM.Web/Program.cs
+++ src/PM.Web/Program.cs
@@ -1,16 +1,34 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using PM.DAL;
 using PM.DAL.Domain.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentity<User, Role>()
-    .AddEntityFrameworkStores<EntityContext>()
-    .AddDefaultTokenProviders();
+builder.Services.AddDbContext<EntityContext>(opt =>
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<User, Role>(o =>
+    {
+        o.Stores.MaxLengthForKeys = 128;
+        o.SignIn.RequireConfirmedAccount = true;
+    })
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
