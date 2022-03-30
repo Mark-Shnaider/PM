866d9dc74a693ca4d35db423a68a185d06010e2e
Mark Shnayder <mark.shnayder@sibers.com>
2022-03-30 16:40:42 +0700

    Add identity

--- src/PM.Web/Program.cs
+++ src/PM.Web/Program.cs
@@ -5,32 +5,26 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using PM.DAL;
 using PM.DAL.Domain.Models.Identity;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<EntityContext>(opt =>
-	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<EntityContext>(opt => 
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<User, Role>(o =>
-    {
-        o.Stores.MaxLengthForKeys = 128;
-        o.SignIn.RequireConfirmedAccount = true;
-    })
+builder.Services.AddIdentity<User, Role>()
+    .AddEntityFrameworkStores<EntityContext>()
+    .AddSignInManager<User>()
+    .AddUserManager<User>()
     .AddDefaultTokenProviders();
 
+
+
+
 var app = builder.Build();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
